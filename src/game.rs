aa715bac57f8b5c6bdba5a34ec33aa9a8f7037b9
CRBroughton <crbroughton@posteo.uk>
2022-07-27 14:48:41 +0100

    feat(game): add frame timing to game

--- src/game.rs
+++ src/game.rs
@@ -4,17 +4,23 @@ use crate::wasm4;
 
 pub struct Game {
     snake: Snake,
+    frame_count: u32
 }
 
 impl Game {
     pub fn new() -> Self {
         Self {
             snake: Snake::new(),
+            frame_count: 0,
         }
     }
 
     pub fn update(&mut self) {
-        self.snake.update();
+        self.frame_count += 1;
+        
+        if self.frame_count % 15 == 0 {
+            self.snake.update();
+        }
         self.snake.draw();
     }
 }
